8bf04de5f767179c2a1b04b064e1dc95fd923016
omar abdo <omarfaroq2003@gmail.com>
2025-10-10 15:12:17 +0300

    done

---
+++ src/Infrastructure/Migrations/20251009184324_foreignKey.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class foreignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_AspNetUsers_UserId",
+                table: "Meals");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_AspNetUsers_UserId",
+                table: "Meals",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meals_AspNetUsers_UserId",
+                table: "Meals");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meals_AspNetUsers_UserId",
+                table: "Meals",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
